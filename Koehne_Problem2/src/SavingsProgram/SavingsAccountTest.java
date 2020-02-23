1a441d76cad6465df00e23e185c4accb911e7015
unknown <thomaskoehnej@gmail.com>
2020-02-23 15:46:20 -0500

    Working Solution for Problem 2

--- Koehne_Problem2/src/SavingsProgram/SavingsAccountTest.java
+++ Koehne_Problem2/src/SavingsProgram/SavingsAccountTest.java
@@ -3,8 +3,30 @@ package SavingsProgram;
 public class SavingsAccountTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SavingsAccount saver1 = new SavingsAccount();
+		SavingsAccount saver2 = new SavingsAccount();
 
+		saver1.setSavingsBalance(2000.00);
+		saver2.setSavingsBalance(3000.00);
+		
+		SavingsAccount.modifyInterestRate(0.04);
+		
+		System.out.printf("Inital Balance:%nSaver1: %.2f     Saver2: %.2f%n", saver1.getSavingsBalance(), saver2.getSavingsBalance());
+		System.out.printf("12 Months of %.0f%% Annual Interest Monthly:%n", (SavingsAccount.getAnnualInterestRate() * 100));
+		for(int i = 0; i < 12; i++) {
+			saver1.calculateMonthlyInterest();
+			saver2.calculateMonthlyInterest();
+			System.out.printf("Month %d: Saver1: %.2f     Saver2: %.2f%n", (i + 1), saver1.getSavingsBalance(), saver2.getSavingsBalance());
+		}
+		
+		SavingsAccount.modifyInterestRate(0.05);
+		
+		System.out.printf("12 Months of %.0f%% Annual Interest Monthly:%n", (SavingsAccount.getAnnualInterestRate() * 100));
+		for(int i = 0; i < 12; i++) {
+			saver1.calculateMonthlyInterest();
+			saver2.calculateMonthlyInterest();
+			System.out.printf("Month %d: Saver1: %.2f     Saver2: %.2f%n", (i + 1), saver1.getSavingsBalance(), saver2.getSavingsBalance());
+		}
 	}
 
 }
